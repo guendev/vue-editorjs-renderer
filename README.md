aa4a196e082e227cdf03e6c2f57d55dc4af9c384
Forek <dnstylish@gmail.com>
2020-04-21 17:42:00 +0700

    Fix

--- README.md
+++
@@ -1,2 +0,0 @@
-## Demo
-<p align="center"><img src="https://i.imgur.com/7DMlqScr.jpg"></p>
--- resources/js/tool/editor.js
+++
@@ -1,6 +0,0 @@
-
-import Vue from "vue";
-Vue.component('editor', require('../components/Editor').default);
-const app = new Vue({
-    el: '#app-frontend'
-});
